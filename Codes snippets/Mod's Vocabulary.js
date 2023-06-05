b822551047b549894c826344effc4de554eaf877
TheGreatMegalodon <69216391+TheGreatMegalodon@users.noreply.github.com>
2023-06-05 21:10:34 +0200

    Update Mod's Vocabulary.js

--- Codes snippets/Mod's Vocabulary.js
+++ Codes snippets/Mod's Vocabulary.js
@@ -1,9 +1,9 @@
 const vocabulary = [
-  { text: "You", icon: "\u004e", key: "O"},
-  { text: "Me", icon: "\u004f", key: "E"},
-  { text: "Wait", icon: "\u0048", key: "T"},
-  { text: "Yes", icon: "\u004c", key: "Y"},
-
+  {text: "You", icon: "\u004e", key: "O"},
+  {text: "Me", icon: "\u004f", key: "E"},
+  {text: "Wait", icon: "\u0048", key: "T"},
+  {text: "Yes", icon: "\u004c", key: "Y"},
+  
   {text: "No", icon: "\u004d", key: "N"},
   {text: "Hello", icon: "\u0045", key: "H"},
   {text: "Sorry", icon: "\u00a1", key: "S"},
@@ -12,14 +12,15 @@ const vocabulary = [
   {text: "Attack", icon: "\u0049", key: "A"},
   {text: "Follow Me", icon: "\u0050", key: "F"},
   {text: "Good Game", icon: "\u00a3", key: "G"},
-  {text: "Spectate", icon: "\u0059", key: "L"},
+  {text: "Leave", icon: "\u00b3", key: "L"},
 
   {text: "Gems", icon: "\u0044", key: "M"},
   {text: "Stats", icon: "\u0078", key: "K"},
   {text: "Hmm", icon: "\u004b", key: "Q"},
   {text: "No Prb", icon: "\u0047", key: "P"},
-
+  
   {text: "Discord", icon: "\u007b", key: "D"},
   {text: "Idiot", icon: "\u0079", key: "I"},
-  {text: "Lag", icon: "\u0069", key: "J"}
+  {text: "Lag", icon: "\u0069", key: "J"},
+  {text: "Spectate", icon: "\u0059", key: "W"}
 ];
